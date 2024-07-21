a864948f38246d85db723cd4151c6ab5bf62a886
algok <2219444825@qq.com>
2024-07-21 23:00:47 +0800

    deploy

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/blog',
+  base: '/',
   title: "ww's blog",
   description: "blog",
   themeConfig: {
